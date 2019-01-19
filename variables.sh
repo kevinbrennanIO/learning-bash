0cec36be7a701acc8b982d0e2ad821acfb4e0103
Kevin <kevdj2000@gmail.com>
2019-01-19 17:52:55 +0000

    #

--- variables.sh
+++ variables.sh
@@ -7,3 +7,18 @@ day=$(date +%A)
 echo "$greeting back $user! Today is $day, which is the best day of the entire week!!"
 echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
 
+a=4
+b=5
+
+echo Values: $a + $b = $[$a + $b]
+
+
+# This part of the script is used to backup a user's home directory to /tmp/.
+
+user=$(whoami)
+input=/Users/$user/Desktop
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+
+tar -czf $output $input
+echo "Backup of $input completed! Details about the output backup file:"
+ls -l $output
