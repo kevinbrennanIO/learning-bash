d7ca57d4d6a46578a3f13f89bc8e6ee01c3edd29
Kevin <kevdj2000@gmail.com>
2019-01-20 16:55:09 +0000

    #

--- basic_logic.sh
+++ basic_logic.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-let var=5
+let var=20
+
+# logical or
 
 if [ "$var" -gt 10 ] || [ "$var" -eq 20 ]
 then 
 	echo "You got a match"
 else 
 	echo "No match this time"
+fi
+
+# logical and
+
+if [ "$var" -gt 10 ] && [ "$var" -eq 20 ]
+then
+        echo "You got a match"
+else
+        echo "No match this time"
 fi
 
